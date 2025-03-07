4816888c8cd18db3c374c5c59ce5e35f40d7f956
runeschristensen <runeschristensen@gmail.com>
2025-03-07 15:32:26 +0100

    getAllRequest

--- 6.SDK/Movies.Api.Sdk.Consumer/Program.cs
+++ 6.SDK/Movies.Api.Sdk.Consumer/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Components.Web;
+using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
+using Refit;
+
+var moviesApi = RestService.For<IMoviesApi>("https//localhost:5001");
+
+var movie = await moviesApi.GetMovieAsync("test");
+
+var request = new GetAllMoviesRequest 
+{
+    Title = null,
+    Year = null,
+    SortBy= null,
+    Page = 1,
+    PageSize = 3
+};
+
+var movies = await moviesApi.GetMoviesAsync(request);
+
+Console.WriteLine(JsonSerializer.Serialize(movie));
